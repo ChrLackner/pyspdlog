e1f34873fc198b8a82f3b9ecf96ba7a6fa715024
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-07-04 14:13:42 +0200

    fix package config setup

--- cmake/PyspdlogConfig.cmake
+++ cmake/PyspdlogConfig.cmake
@@ -1,1 +1,4 @@
 include("${CMAKE_CURRENT_LIST_DIR}/PyspdlogTargets.cmake")
+
+set(PYSPDLOG_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/pyspdlog/include ${CMAKE_INSTALL_PREFIX}/pyspdlog/include/spdlog)
+set(PYSPDLOG_LIBRARIES ${CMAKE_INSTALL_PREFIX}/pyspdlog/libpyspdlog.so)
