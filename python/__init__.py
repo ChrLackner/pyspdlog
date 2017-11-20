1ff6292a91ce17abda93d19ae85da3f027b6f44f
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-11-20 09:41:58 +0100

    add a function to create log files for each thread

--- python/__init__.py
+++ python/__init__.py
@@ -1,2 +1,18 @@
 
 from pyspdlog.libpyspdlog import *
+import os,shutil
+
+def CreateThreadLogFiles(logfile):
+    """Create seperate logfiles for each thread, the thread number must be the first column in the
+    logfile."""
+
+    filename = "".join(logfile.split(".")[:-1])
+    extension = logfile.split(".")[-1]
+    if os.path.exists(filename + "_thread"):
+        shutil.rmtree(filename + "_thread")
+    os.mkdir(filename + "_thread")
+    with open(logfile,'r') as f:
+        for line in f:
+            threadnr = line.split(" ")[0]
+            with open(filename + "_thread/" + threadnr + "." + extension,"a") as fw:
+                print(line[:-1],file=fw)
